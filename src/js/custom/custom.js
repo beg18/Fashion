15e5c0742d19d27d05225ba9be18bd578402aae3
Alibekov Khiyirbeg <beg_geb@mail.ru>
2019-02-14 20:13:43 +0300

    Add new project Fashion

---
+++ src/js/custom/custom.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+    $('nav').coreNavigation({
+        menuPosition: "center",
+        container: true
+    });
+
+    $('.header__slider .owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots: false,
+        navText:[],
+        responsive:{
+            0:{
+                items:1,
+                dots:true,
+                nav:false
+            },
+            480:{
+                items:1
+            }
+        }
+    })
+});
+
+
+
---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+/* Libs
+=========================================================================================
+=========================================================================================
+*/
+
+//= ../../bower_components/corenav/assets/js/jquery.min.js
+//= ../../bower_components/jquery-mousewheel/jquery.mousewheel.min.js
+//= ../../bower_components/fancybox/jquery.fancybox.pack.js
+//= ../../bower_components/waypoints/waypoints-1.6.2.min.js
+//= ../../bower_components/animate/animate-css.js
+//= ../../bower_components/scrollto/jquery.scrollTo.min.js
+//= ../../bower_components/owl-carousel/owl.carousel.js
+//= ../../bower_components/countdown/jquery.plugin.js
+//= ../../bower_components/countdown/jquery.countdown.min.js
+//= ../../bower_components/countdown/jquery.countdown-ru.js
+//= ../../bower_components/landing-nav/navigation.js
+//= ../../bower_components/corenav/dist/coreNavigation-1.1.3.min.js
+//= ../../bower_components/equalHeight/jquery.equalheights.min.js
+
+
+/* Custom
+===========================*/
+//= custom/custom.js
+
