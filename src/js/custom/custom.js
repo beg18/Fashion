36748e4d83a15d9003d5c4f53382bf2eb1952491
Alibekov Khiyirbeg <beg_geb@mail.ru>
2019-02-15 16:00:13 +0300

    Carousel added and fully adapted this page.

--- src/js/custom/custom.js
+++ src/js/custom/custom.js
@@ -45,6 +45,27 @@ $(document).ready(function() {
         }
     });
 
+    $('#js_featured .owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots: false,
+        navText:[],
+        responsive:{
+            0:{
+                items:1,
+            },
+            480:{
+                items:1
+            },
+            680:{
+                items:2
+            },
+            1024:{
+                items:4
+            },
+        }
+    });
 });
 
 
